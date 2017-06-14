1832dd0d3ca1f6e53c8eeb2e6af988f0e04704b0
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2017-06-13 19:35:46 -0700

    finish phone number

--- src/MaskedInput.js
+++ src/MaskedInput.js
@@ -33,8 +33,17 @@ class MaskedInput extends Component {
    */
 	handleKeyDown(event) {
 		if (event.keyCode === 8)  { // Backspace
-			console.log('backspace');
-			this.setState({ realValue: this.state.realValue.slice(0, -1) });
+			if (this.preMasked) {
+				if (this.inputType === "dob") {
+					this.deleteFromDOB(event);
+				} else if (this.inputType === "phone") {
+					this.deleteFromPhone(event);
+				} else { // SSN
+					this.deleteFromSSN(event); 
+				}
+			} else {
+				this.setState({ realValue: this.state.realValue.slice(0, -1) });				
+			}
 		} else {
 			if (event.target.value.length == this.props.length) {
 				event.preventDefault(); // Make sure we don't append the new letter onto the form's value
@@ -102,30 +111,29 @@ class MaskedInput extends Component {
 		if (event.target.value.length === 0) {
 			if (this.preMasked) {
 				this.insertAtIndex = 1;
-				this.setState({ realValue: realValue.substr(0, this.insertAtIndex) + event.key + realValue.substr(this.insertAtIndex + 1, realValue.length )});
+				this.setState({ realValue: realValue.substr(0, this.insertAtIndex) + event.key + realValue.substr(this.insertAtIndex + 1, realValue.length) });
+				this.insertAtIndex++;
 			} else {
 				this.setState({ realValue: '(' + event.key });				
 			}
 		} else if (event.target.value.length === 2) {
 			if (this.preMasked) {
-				this.insertAtIndex++;
-				this.setState({ realValue: realValue.substr(0, this.insertAtIndex) + event.key + realValue.substr(this.insertAtIndex + 1, realValue.length )});
-				this.insertAtIndex += 2;
+				this.setState({ realValue: realValue.substr(0, this.insertAtIndex) + event.key + realValue.substr(this.insertAtIndex + 1, realValue.length) });
+				this.insertAtIndex += 3;
 			} else {
 				this.setState({ realValue: this.state.realValue + event.key + ') ' });				
 			}
 		} else if (event.target.value.length === 5) {
 			if (this.preMasked) {
-				this.insertAtIndex++;
-				this.setState({ realValue: realValue.substr(0, this.insertAtIndex) + event.key + realValue.substr(this.insertAtIndex + 1, realValue.length )});
-				this.insertAtIndex++;
+				this.setState({ realValue: realValue.substr(0, this.insertAtIndex) + event.key + realValue.substr(this.insertAtIndex + 1, realValue.length) });
+				this.insertAtIndex += 2;
 			} else {
 				this.setState({ realValue: this.state.realValue + event.key + '-' });				
 			}
 		} else {
 			if (this.preMasked) {
+				this.setState({ realValue: realValue.substr(0, this.insertAtIndex) + event.key + realValue.substr(this.insertAtIndex + 1, realValue.length) });
 				this.insertAtIndex++;
-				this.setState({ realValue: realValue.substr(0, this.insertAtIndex) + event.key + realValue.substr(this.insertAtIndex + 1, realValue.length )});
 			} else {
 				this.setState({ realValue: this.state.realValue + event.key });				
 			}
@@ -138,10 +146,33 @@ class MaskedInput extends Component {
 	 */
 	formatSSN(event) {
 		if (event.target.value.length === 2 || event.target.value.length === 4) {
-			this.setState({ realValue: this.state.realValue + event.key + '-'})
+			this.setState({ realValue: this.state.realValue + event.key + '-' });
 		} else {
 			this.setState({ realValue: this.state.realValue + event.key });
 		}
+	}
+
+	deleteFromDOB(event) {
+		
+	}
+
+	deleteFromPhone(event) {
+		var realValue = this.state.realValue;
+
+		if (event.target.value.length === 3) {
+			this.setState({ realValue: realValue.substr(0, this.insertAtIndex - 3) + 'X' + realValue.substr(this.insertAtIndex - 2, realValue.length) });
+			this.insertAtIndex -= 3;			
+		} else if (event.target.value.length === 6) {
+			this.setState({ realValue: realValue.substr(0, this.insertAtIndex - 2) + 'X' + realValue.substr(this.insertAtIndex - 1, realValue.length) });
+			this.insertAtIndex -= 2;
+		} else {
+			this.setState({ realValue: realValue.substr(0, this.insertAtIndex - 1) + 'X' + realValue.substr(this.insertAtIndex, realValue.length) });
+			this.insertAtIndex--;
+		}
+	}
+
+	deleteFromSSN(event) {
+
 	}
 
   render() {
